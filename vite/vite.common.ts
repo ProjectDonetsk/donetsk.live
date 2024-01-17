296741db1c19e9b30514fead0e6cc38a9f0d6b9b
Damien Vesper <ldamienvesper@gmail.com>
2024-01-17 11:47:04 -0600

    chore: vite updates, bump deps

--- vite/vite.common.ts
+++ vite/vite.common.ts
@@ -1,12 +1,17 @@
 import pkg from '../package.json';
 
-import type { UserConfig } from 'vite';
-
+import { splitVendorChunkPlugin, type UserConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 const config: UserConfig = {
     plugins: [
-        svelte()
+        svelte(),
+        splitVendorChunkPlugin(),
+        ViteImageOptimizer({
+            test: /\.(svg)$/i,
+            logStats: false
+        })
     ],
 
     define: {
--- vite/vite.dev.ts
+++ vite/vite.dev.ts
@@ -5,11 +5,13 @@ import { mergeConfig, type UserConfig } from 'vite';
 const config: UserConfig = {
     server: {
         port: 3000,
-        strictPort: true
+        strictPort: true,
+        host: `0.0.0.0`
     },
     preview: {
         port: 3000,
-        strictPort: true
+        strictPort: true,
+        host: `0.0.0.0`
     },
 
     define: {
